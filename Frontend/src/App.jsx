ffa12f53964d081a4792d20389c5b146c3e27dff
JOSEPABLOGH <jpgalindoh@gmail.com>
2026-03-25 16:49:21 -0600

    Backend y Frontend conectados

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/test')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data) // 👈 clave
+        setData(data)
+      })
+  }, [])
+
   return (
-    <h1>TALK
-    </h1>
+    <div>
+      <h1>Frontend prueba</h1>
+      <p>{data ? data.mensaje : 'Cargando...'}</p>
+    </div>
   )
 }
 
